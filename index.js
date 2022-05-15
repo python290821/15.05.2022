9ebd823d95d1a5c6690eb1f0a01c86e5e26aa0e5
micserv010821 <dotnet290320@gmail.com>
2022-05-15 19:03:22 +0300

    upload all

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const path = require('path')
 
 logger.debug('====== System startup ========')
 
-//logger.error(`cannot close db. Error: ${err}`)
-
 const port = 8080
 
 const app = express()
@@ -20,6 +18,64 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/movies', async(req, res) => {
     const movies = await moveis_repo.getAllMovies()
     res.status(200).json( {movies })
+})
+
+app.get('/movies/:movie_id', async(req, res) => {
+    const movie_id = req.params.movie_id
+    const movies = await moveis_repo.getMovieByid(movie_id)
+    res.status(200).json( {movies })
+})
+
+app.delete('/movies/:movie_id', async(req, res) => {
+    const movie_id = req.params.movie_id
+    try {
+        const movies = await moveis_repo.deleteMovie(movie_id)
+        res.status(200).json( { num_records_deleted: movies })
+    }
+    catch (e) {
+        logger.error(`failed to delete movie. Error: ${e}`)
+        res.status(400).send({
+            status: 'error',
+            message: e.message
+        })
+    }
+})
+
+app.put('/movies/:movie_id', async(req, res) => {
+    const movie_id = req.params.movie_id
+    try {
+        movie = req.body
+        const result = await moveis_repo.updateMovie(movie, movie_id)
+        res.status(200).json( {
+            res: 'success',
+            url: `/movies/${movie.ID}`,
+            result
+             })
+    } catch (e) {
+        logger.error(`failed to update movie. Error: ${e}`)
+        res.status(400).send({
+            status: 'error',
+            message: e.message
+        })
+    }
+})
+
+app.post('/movies', async(req, res) => {
+    try {
+        movie = req.body
+        const result = await moveis_repo.addMovie(movie)
+        res.status(201).json( {
+            res: 'success',
+            url: `/movies/${result[0]}`,
+            result
+             })
+    } catch (e) {
+        logger.error(`failed to insert movie. Error: ${e}`)
+        res.status(400).send({
+            status: 'error',
+            message: e.message
+        })
+    }
 })
 
 app.listen(port, () => {
--- movies-repo.js
+++ movies-repo.js
@@ -8,6 +8,26 @@ function getAllMovies() {
     return connectedKnex('PRODUCTS').select('*');
 }
 
+function getMovieByid(id) {
+    return connectedKnex('PRODUCTS').select('*').where('id', id).first();
+}
+
+function addMovie(movie) {
+    return connectedKnex('PRODUCTS').insert(movie);
+}
+
+function updateMovie(movie, id) {
+    return connectedKnex('PRODUCTS').where('id', id).update(movie);
+}
+
+function deleteMovie(id) {
+    return connectedKnex('PRODUCTS').where('id', id).del();
+}
+
 module.exports = {
-    getAllMovies
+    getAllMovies,
+    getMovieByid,
+    addMovie,
+    updateMovie,
+    deleteMovie
 }
